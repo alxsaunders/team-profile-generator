65d7e235132c6a44ef5c2249b7ec77321090daea
alxsaunders <alexsaunders242@gmail.com>
2022-07-18 18:51:16 -0400

    start index.js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const inquirer = require('inquirer')
 const template = require('./src/page-template')
-const writeFile = require('./src/write-file')
+const writeFile = require('./src/writie-file')
+
+// constructor classes and questions for each employee type
+const { Manager, managerQuestionsArr } = require('./lib/Manager');
+const { Engineer, engineerQuestionsArr } = require('./lib/Engineer');
+const { Intern, internQuestionsArr } = require('./lib/Intern');
+
+// stores all team member objects
+const employeesArr = []
+
+const init = () => { managerQuestions() }
+// prompts manager questions then creates object from user inputs based on Manager class 
+const managerQuestions = () => {
+    inquirer.prompt(managerQuestionsArr)
+    .then(( answers ) => {
+        answers = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+        employeesArr.push(answers);
+        return employeePrompt();
+    })
+}
+
